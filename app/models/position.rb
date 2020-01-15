048f009286b8994698b148d397be8ed24a2b0932
Philippe Gilles <philgdep@gmail.com>
2020-01-15 14:56:16 +0100

    add delete_all dependency

--- app/models/position.rb
+++ app/models/position.rb
@@ -1,5 +1,5 @@
 class Position < ApplicationRecord
-  has_many :position_skill
+  has_many :position_skill, dependent: :delete_all
   has_many :skills, through: :position_skill
   validates :title, presence: true, length: {minimum:3, maximum:50}
   validates :description, presence: true, length: {minimum:10, maximum:10000}
--- app/models/recruiter.rb
+++ app/models/recruiter.rb
@@ -1,4 +1,4 @@
 class Recruiter < ApplicationRecord
-    has_many :recruiter_skill
+    has_many :recruiter_skill, dependent: :delete_all
     has_many :skills, through: :recruiter_skill
 end
