1498b2c41cdfe969d9b15f6ee13591a7e638aad3
Philippe Gilles <philgdep@gmail.com>
2020-01-15 16:47:00 +0100

    fix candidate model association

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -1,5 +1,5 @@
 class Candidate < ApplicationRecord
     has_many :candidate_skill, dependent: :delete_all
     has_many :skills, through: :candidate_skill
-    belongs_to :recruiter
+    belongs_to :recruiter, optional: true
 end
--- app/models/recruiter.rb
+++ app/models/recruiter.rb
@@ -1,4 +1,5 @@
 class Recruiter < ApplicationRecord
     has_many :recruiter_skill, dependent: :delete_all
     has_many :skills, through: :recruiter_skill
+    has_many :candidates
 end
