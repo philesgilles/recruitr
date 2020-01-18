d167c0405b2a2e2a067ee3c791c4318f9774976b
Philippe Gilles <philgdep@gmail.com>
2020-01-18 15:38:58 +0100

    a

--- db/migrate/20200118105443_create_meeting_table.rb
+++ db/migrate/20200118105443_create_meeting_table.rb
@@ -1,6 +1,6 @@
 class CreateMeetingTable < ActiveRecord::Migration[6.0]
   def change
-    create_table :meetings do |t|
+    create_table :meeting_tables do |t|
       t.string :place
       t.datetime :time
       t.references :recruiter, foreign_key: true
