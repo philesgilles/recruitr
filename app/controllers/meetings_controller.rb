1b135efd53d215d472d4a29e99d92f6c9a609acb
Philippe Gilles <philgdep@gmail.com>
2020-01-18 16:17:49 +0100

    add, create and show meetings ok

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -1,14 +1,34 @@
 class MeetingsController < ApplicationController
-    before_action :set_candidate, only:[:new]
+    before_action :set_meeting, only:[:edit,:update,:show,:destroy]
     before_action :require_user
     def index
         
     end
     def new
+        @candidate = Candidate.find(params[:id])
+        @meeting = Meeting.new
+    end
+    def create
+        debugger
+        @meeting = Meeting.new(meeting_params)
+        if @meeting.save
+            flash[:success] = "created"
+            redirect_to meeting_path(@meeting) 
+        else
+            render 'new'
+        end
+    end
+    def show
+        
     end
     
+    
+    
     private
-    def set_candidate
-        @candidate = Candidate.find(params[:id])
+    def meeting_params
+        params.require(:meeting).permit(:time,:place,:recruiter_id,:candidate_id)
+    end
+    def set_meeting
+        @meeting = Meeting.find(params[:id])
     end
 end
